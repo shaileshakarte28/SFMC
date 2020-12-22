8d2abeb33e82efdc1b8b4d49a1c44dd727478aff
Shailesh Akarte <Shailesh-Akarte@na.odcorp.net>
2020-12-23 00:19:51 +0530

    Some Change

--- app.js
+++ app.js
@@ -27,36 +27,16 @@ if ('development' == app.get('env')) {
   app.use(errorhandler());
 }
 
-
-app.get('/', callName); 
-  
-function callName(req, res) { 
-      
-    // Use child_process.spawn method from  
-    // child_process module and assign it 
-    // to variable spawn 
-    var spawn = require("child_process").spawn; 
-      
-    // Parameters passed in spawn - 
-    // 1. type_of_script 
-    // 2. list containing Path of the script 
-    //    and arguments for the script  
-      
-    // E.g : http://localhost:3000/name?firstname=Mike&lastname=Will 
-    // so, first name = Mike and last name = Will 
-    var process = spawn('python',["/test.py"] ); 
-  
-
 // HubExchange Routes
-// app.get('/', routes.index );
-// app.post('/login', routes.login );
-// app.post('/logout', routes.logout );
+app.get('/', routes.index );
+app.post('/login', routes.login );
+app.post('/logout', routes.logout );
 
-// // Custom Hello World Activity Routes
-// app.post('/journeybuilder/save/', activity.save );
-// app.post('/journeybuilder/validate/', activity.validate );
-// app.post('/journeybuilder/publish/', activity.publish );
-// app.post('/journeybuilder/execute/', activity.execute );
+// Custom Hello World Activity Routes
+app.post('/journeybuilder/save/', activity.save );
+app.post('/journeybuilder/validate/', activity.validate );
+app.post('/journeybuilder/publish/', activity.publish );
+app.post('/journeybuilder/execute/', activity.execute );
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
