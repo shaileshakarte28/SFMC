871749b0c8fbf8c4f8a583f02d44103e1786254c
Shailesh Akarte <Shailesh-Akarte@na.odcorp.net>
2020-12-23 00:39:34 +0530

    Some Change

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -130,7 +130,7 @@
 
 const spawn = require(['child_process']).spawn;
  
-const process = spawn('python', ['./test.py']);
+const process = spawn('python', ['./test.py',]);
  
 process.stdout.on('data',data => {
     console.log('Test1');
