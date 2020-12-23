83a32d9c8198ee2b2add3ce98e40bfd109dd977a
Shailesh Akarte <Shailesh-Akarte@na.odcorp.net>
2020-12-23 15:07:36 +0530

    new script added

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -1,104 +1,104 @@
-define([
-    'postmonger'
-], function (
-    Postmonger
-) {
-    'use strict';
+// define([
+//     'postmonger'
+// ], function (
+//     Postmonger
+// ) {
+//     'use strict';
 
-    var connection = new Postmonger.Session();
-    var authTokens = {};
-    var payload = {};
-    $(window).ready(onRender);
+//     var connection = new Postmonger.Session();
+//     var authTokens = {};
+//     var payload = {};
+//     $(window).ready(onRender);
     
     
-    connection.on('initActivity', initialize);
-    connection.on('requestedTokens', onGetTokens);
-    connection.on('requestedEndpoints', onGetEndpoints);
+//     connection.on('initActivity', initialize);
+//     connection.on('requestedTokens', onGetTokens);
+//     connection.on('requestedEndpoints', onGetEndpoints);
 
-    connection.on('clickedNext', save);
+//     connection.on('clickedNext', save);
    
-    function onRender() {
-        // JB will respond the first time 'ready' is called with 'initActivity'
-        connection.trigger('ready');
-        connection.trigger('requestTokens');
-        connection.trigger('requestEndpoints');
-        pyData();
-    }
+//     function onRender() {
+//         // JB will respond the first time 'ready' is called with 'initActivity'
+//         connection.trigger('ready');
+//         connection.trigger('requestTokens');
+//         connection.trigger('requestEndpoints');
+//         pyData();
+//     }
 
-    function pyData() {
-      const { spawn } = require('child_process');
-      const temperatures = []; // Store readings
+//     function pyData() {
+//       const { spawn } = require('child_process');
+//       const temperatures = []; // Store readings
     
-      const sensor = spawn('python', ['test.py']);
-      sensor.stdout.on('data', function(data) {
+//       const sensor = spawn('python', ['test.py']);
+//       sensor.stdout.on('data', function(data) {
     
-        // convert Buffer object to Float
-        temperatures.push(parseFloat(data));
-        console.log(temperatures);
-    });
-        // const spawn = require(["child_process"]).spawn; 
-        // const process = spawn('python',["./test.py"] ); 
-        // console.log("Inside pyData Function...!!");
+//         // convert Buffer object to Float
+//         temperatures.push(parseFloat(data));
+//         console.log(temperatures);
+//     });
+//         // const spawn = require(["child_process"]).spawn; 
+//         // const process = spawn('python',["./test.py"] ); 
+//         // console.log("Inside pyData Function...!!");
         
-    }
+//     }
 
-    function initialize(data) {
-        console.log(data);
-        if (data) {
-            payload = data;
-        }
+//     function initialize(data) {
+//         console.log(data);
+//         if (data) {
+//             payload = data;
+//         }
         
-        var hasInArguments = Boolean(
-            payload['arguments'] &&
-            payload['arguments'].execute &&
-            payload['arguments'].execute.inArguments &&
-            payload['arguments'].execute.inArguments.length > 0
-        );
+//         var hasInArguments = Boolean(
+//             payload['arguments'] &&
+//             payload['arguments'].execute &&
+//             payload['arguments'].execute.inArguments &&
+//             payload['arguments'].execute.inArguments.length > 0
+//         );
 
-        var inArguments = hasInArguments ? payload['arguments'].execute.inArguments : {};
+//         var inArguments = hasInArguments ? payload['arguments'].execute.inArguments : {};
 
-        console.log(inArguments);
+//         console.log(inArguments);
 
-        $.each(inArguments, function (index, inArgument) {
-            $.each(inArgument, function (key, val) {
+//         $.each(inArguments, function (index, inArgument) {
+//             $.each(inArgument, function (key, val) {
                 
               
-            });
-        });
+//             });
+//         });
 
-        connection.trigger('updateButton', {
-            button: 'next',
-            text: 'done',
-            visible: true
-        });
-    }
+//         connection.trigger('updateButton', {
+//             button: 'next',
+//             text: 'done',
+//             visible: true
+//         });
+//     }
 
-    function onGetTokens(tokens) {
-        console.log(tokens);
-        authTokens = tokens;
-    }
+//     function onGetTokens(tokens) {
+//         console.log(tokens);
+//         authTokens = tokens;
+//     }
 
-    function onGetEndpoints(endpoints) {
-        console.log(endpoints);
-    }
+//     function onGetEndpoints(endpoints) {
+//         console.log(endpoints);
+//     }
 
-    function save() {
-        var postcardURLValue = $('#postcard-url').val();
-        var postcardTextValue = $('#postcard-text').val();
+//     function save() {
+//         var postcardURLValue = $('#postcard-url').val();
+//         var postcardTextValue = $('#postcard-text').val();
 
-        payload['arguments'].execute.inArguments = [{
-            "tokens": authTokens,
-            "emailAddress": "{{Contact.Attribute.PostcardJourney.EmailAddress}}"
-        }];
+//         payload['arguments'].execute.inArguments = [{
+//             "tokens": authTokens,
+//             "emailAddress": "{{Contact.Attribute.PostcardJourney.EmailAddress}}"
+//         }];
         
-        payload['metaData'].isConfigured = true;
+//         payload['metaData'].isConfigured = true;
 
-        console.log(payload);
-        connection.trigger('updateActivity', payload);
-    }
+//         console.log(payload);
+//         connection.trigger('updateActivity', payload);
+//     }
 
 
-});
+// });
 
 
 // const spawn = require(['child_process']).spawn;
@@ -126,7 +126,7 @@ $(document).ready(function () {
 
     
 
-    xmlhttp.onreadystatechange = callbackFunction(xmlhttp);
+    // xmlhttp.onreadystatechange = callbackFunction(xmlhttp);
     xmlhttp.open("POST",url, false);
     xmlhttp.setRequestHeader("Content-type", "application/json;charset=UTF-8");
     xmlhttp.send(jsonPayload);
